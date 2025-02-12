5437241c1e4c54ca02e3658156b7a32d3b35cf5e
dgoceva <44052125+dgoceva@users.noreply.github.com>
2025-02-12 11:59:00 +0200

    12-02-2025

--- myfirstproject/main.c
+++ myfirstproject/main.c
@@ -1,15 +1,129 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <ctype.h>
 
 #define LEN 100
 
-int main()
+void upperCase(char str[])
+{
+    for (unsigned i=0;i<strlen(str);++i){
+        str[i] = toupper(str[i]);
+    }
+}
+void lowerCase(char str[])
+{
+   for (unsigned i=0;i<strlen(str);++i){
+        str[i] = tolower(str[i]);
+   }
+}
+void sentenceCase(char str[])
+{
+    str[0] = toupper(str[0]);
+    for (unsigned i=1;i<strlen(str);++i){
+        str[i] = tolower(str[i]);
+   }
+}
+void workWithNames()
 {
     char name[LEN];
     fgets(name,LEN,stdin);
     name[strlen(name)-1]='\0';
     puts(name);
+    upperCase(name);
     printf("Hello world,%s!\n",name);
+    sentenceCase(name);
+    printf("Hello world,%s!\n",name);
+    lowerCase(name);
+    printf("Hello world,%s!\n",name);
+}
+
+void fillWithZero(unsigned counter[],unsigned len)
+{
+    for (unsigned i=0;i<len;++i){
+        counter[i] = 0;
+    }
+}
+
+void count(char line[], unsigned counter[])
+{
+    char temp[strlen(line)];
+    strcpy(temp,line);
+    upperCase(temp);
+    for (unsigned i=0;i<strlen(line);++i){
+        if (isalpha(temp[i])){
+            ++counter[temp[i]-'A'];
+        }
+    }
+}
+
+void outputCounter(unsigned counter[],unsigned len)
+{
+    for (unsigned i=0;i<len;++i){
+        printf("%c->%u\t",'A'+i,counter[i]);
+    }
+    printf("\n");
+}
+
+unsigned countWords(char line[])
+{
+    const unsigned IN=1,OUT=0;
+    unsigned state=OUT;
+    unsigned words=0;
+
+    for (unsigned i=0;i<strlen(line);++i){
+        if (line[i] == ' ' || line[i] == '\n' || line[i] == '\t')
+            state = OUT;
+        else if (state == OUT) {
+            state = IN;
+            ++words;
+        }
+    }
+    return words;
+}
+
+unsigned countLines(char line[])
+{
+    unsigned lines=1;
+
+    for (unsigned i=0;i<strlen(line);++i){
+        if (line[i]=='\n')
+            ++lines;
+    }
+    return lines;
+}
+
+void workWithLines()
+{
+    const unsigned SIZE=10000,LETTERS=26;
+    char buffer[LEN], line[SIZE];
+    unsigned counter[LETTERS];
+
+    fillWithZero(counter,LETTERS);
+//    outputCounter(counter,LETTERS);
+    while (fgets(buffer,LEN,stdin)!=NULL){
+            strcat(line,buffer);
+            buffer[strlen(buffer)-1]='\0';
+    }
+ //    line[strlen(line)-1]='\0';
+    printf("\n");
+    lowerCase(line);
+    printf("\n");
+    upperCase(line);
+    puts(line);
+    printf("\n");
+    sentenceCase(line);
+    puts(line);
+    printf("\n%u chars in a text\n",strlen(line));
+    printf("%u words in a text\n",countWords(line));
+    printf("%u lines in a text\n\n",countLines(line));
+
+    count(line,counter);
+    outputCounter(counter,LETTERS);
+}
+int main()
+{
+//    workWithNames();
+    workWithLines();
     return 0;
 }
